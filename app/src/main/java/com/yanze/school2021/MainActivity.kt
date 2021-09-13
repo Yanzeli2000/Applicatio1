fad538e78b4e15075f45fcfa145be22fd686ccbf
Yanze Li <jintiandajin4info@gmail.com>
2021-09-12 20:58:29 -0400

    Update MainActivity.kt to bind the view to code, then add event listener await click, then take the text of edit text, then display on screen with Toast for a few second.

--- app/src/main/java/com/yanze/school2021/MainActivity.kt
+++ app/src/main/java/com/yanze/school2021/MainActivity.kt
@@ -2,10 +2,26 @@ package com.yanze.school2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.yanze.school2021.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //Bind the view
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //copy data from the view and assign to binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //Use the binded view instead of raw main view which can't communicate with code
+        setContentView(binding.root)
+        //Give button event listener that await click
+        binding.button.setOnClickListener{
+            //Store edit text 's content
+            var _user_name = binding.editText.text
+            //Call Toast to create text on the screen (target, test, how long to lasting),
+            // and then display it.
+            Toast.makeText(this,"Hello"+_user_name,Toast.LENGTH_LONG).show()
+        }
     }
 }
